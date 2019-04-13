5b1403cf7a09620c09157f23dcaa1168b7783949
kyamashiro <yamashiro@rrr-z.co.jp>
2019-04-13 11:37:36 +0900

    文字列型をクラスによる比較に変更

---
+++ chapter1/src/Wood.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Chapter1;
+
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static Wood INDIAN_ROSEWOOD()
+ * @method static Wood BRAZILIAN_ROSEWOOD()
+ * @method static Wood ALDER()
+ */
+class Wood extends Enum
+{
+    public const INDIAN_ROSEWOOD = 'Indian Rosewood';
+    public const BRAZILIAN_ROSEWOOD = 'Brazilian Rosewood';
+    public const ALDER = 'Alder';
+}
